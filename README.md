9414794035b60366ff29d6cb1665a130614214e7
PIUS <relimoringa@gmail.com>
2023-06-18 00:48:33 +0300

    completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # code-challange-2
+# Speed Limit Checker
+
+This JavaScript code prompts the user to input the speed of a car and checks if it exceeds the speed limit. It then calculates the number of demerit points accumulated and determines if the driver's license should be suspended based on a demerit point threshold.
+
+## Usage
+
+1. Open the JavaScript code in your preferred development environment or browser console.
+2. Run the code.
+3. Enter the speed of the car when prompted.
+4. The program will output one of the following:
+
+   - If the car speed is within the limit: "OK"
+   - If the car speed exceeds the limit: The number of demerit points accumulated
+   - If the demerit points exceed the threshold: "License suspended"
+
---
+++ script.js
@@ -0,0 +1,33 @@
+// Function to check car speed and demerit points
+function checkSpeed() {
+    // Prompt the user for car speed input
+    let carSpeed = parseInt(prompt("INPUT CAR SPEED HERE!"));
+  
+    // Define the demerit points threshold and speed limit
+    let points = 12;
+    let limit = 70;
+  
+    // Calculate the difference between car speed and limit
+    let counter = carSpeed - limit;
+  
+    // Calculate the demerit points per km/h over the limit
+    let counter2 = counter / 5;
+  
+    // Round down the demerit points to a whole number
+    let counter3 = Math.floor(counter2);
+  
+    // Check if car speed is within the limit
+    if (carSpeed < limit) {
+      console.log("OK"); // Car speed is within the limit
+    } else {
+      console.log(counter3); // Output the number of demerit points
+    }
+  
+    // Check if demerit points exceed the threshold for license suspension
+    if (counter3 >= points) {
+      console.log("License suspended"); // License suspension required
+    }
+  }
+  
+  // Call the function to execute the code
+  checkSpeed();
